0d7067b0fb1f0ed149bba650354e94c01224b87c
Timra <timra.work@gmail.com>
2023-01-22 15:31:13 +0500

    SL_4
    https://up.htmlacademy.ru/typescript/1/demos/7273#4

--- src/sanitizer.ts
+++ src/sanitizer.ts
@@ -1,5 +1,9 @@
-import { ERROR_RESULT_IS_RECORD, ERROR_WEATHER_POINTS } from "./errors";
-import { InitialTime, WeatherPoint, WeatherProduct, WeatherResponse } from "./types";
+import { ERROR_RESULT_IS_RECORD, ERROR_WEATHER_POINTS, ERROR_WEATHER_POINT, BUFFER_KEY_NUMBER, VALUE_WEATHER_CODE, INITIAL_TIME_REGEX, WEATHER_PRODUCT } from "./errors";
+import { InitialTime, WeatherPoint, WeatherProduct, WeatherResponse, WeatherCode,
+    weatherCodes,
+    initialTimeRegex,
+    weatherProducts
+} from "./types";
 
 // неизвестный нетривиальный объект наиболее разумно считать словарем
 // из строковых ключей и неизвестных (еще) значений
@@ -8,8 +12,30 @@ const isRecord = (value: unknown): value is Record<string,unknown> => typeof val
 // состоящим из неизвестных значений
 const isArray = (value: unknown): value is unknown[] => Array.isArray(value);
 
+const getNumber = <T>(buffer:Record<string,unknown>, key: string&keyof T): number =>{
+    const value = buffer[key];
+    if(typeof value !== 'number'){
+        throw new TypeError(`${BUFFER_KEY_NUMBER}, key: ${key}`);
+    }
+    return value;
+};
+
+const getWeatherCode = (value: unknown): WeatherCode=>{
+    if(weatherCodes.every((e)=>e!==value)){
+        throw new TypeError(`${VALUE_WEATHER_CODE}, [${value}]`);
+    }
+    return value as WeatherCode;
+};
+
 const getWeatherPointValue = (value: unknown): WeatherPoint=>{
-    throw new Error('not implemented yet');
+    if(!isRecord(value)){
+        throw new TypeError(`${ERROR_WEATHER_POINT}`);
+    }
+    return {
+        temp2m: getNumber<WeatherPoint>(value, 'temp2m'),
+        timepoint: getNumber<WeatherPoint>(value, 'timepoint'),
+        weather: getWeatherCode(value.weather),
+    };
 }
 
 const getWeatherPoint = (value: unknown, ix: number):WeatherPoint =>{
@@ -31,13 +57,19 @@ const getArrayOfWeatherPoints = (value: unknown):WeatherPoint[]=>{
     return value.map(getWeatherPoint);
 }
 
-const getStringOfDigits = (value: unknown): InitialTime =>{
-    throw new Error('not implemented yet');
-}
+const getStringOfDigits = (value: unknown):InitialTime =>{
+    if(typeof value !== 'string' || !initialTimeRegex.test(value)){
+        throw new TypeError(`${INITIAL_TIME_REGEX}, ${value}`);
+    }
+    return value as InitialTime;
+};
 
 const getProductName = (value: unknown): WeatherProduct=>{
-    throw new Error('not implemented yet');
-}
+    if(weatherProducts.every((e)=> e!== value)){
+        throw new TypeError(`${WEATHER_PRODUCT} value: ${value}`);
+    }
+    return value as WeatherProduct;
+};
 
 export const adaptFromJson = (data:unknown):WeatherResponse =>{
     if(!isRecord(data)){
